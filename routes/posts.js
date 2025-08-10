7367226a53f3ddd8b16062f11eee1dd52a31d09a
Taishi1020 <tai.yakiman1020@gmail.com>
2025-08-10 20:09:39 +0900

    投稿いいね機能の実装

--- routes/posts.js
+++ routes/posts.js
@@ -27,5 +27,47 @@ router.put("/:id", async (req, res) => {
   }
 })
 
+// 投稿の削除
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if(post.userId === req.body.userId) {
+      await post.deleteOne();
+      return res.status(200).json("投稿を削除しました。");
+    } else {
+      return res.status(403).json("あなたはこの投稿を削除できません。");
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+})
+
+// 投稿の取得
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    return res.status(200).json(post);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+})
+
+// 投稿のいいね
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    // 投稿にいいねしていない場合
+    if(!post.likes.includes(req.body.userId)) {
+      await post.updateOne({$push: {likes: req.body.userId}});
+      return res.status(200).json("投稿にいいねしました。");
+    } else {
+      await post.updateOne({$pull: {likes: req.body.userId}});
+      return res.status(200).json("投稿のいいねを解除しました。");
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+})
+
 
 module.exports = router;
