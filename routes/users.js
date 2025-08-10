11168eb893e2ea7a5e7a02f21ab4eafef3a87875
Taishi1020 <tai.yakiman1020@gmail.com>
2025-08-10 16:29:09 +0900

    ユーザー情報を削除する処理追加

--- routes/users.js
+++ routes/users.js
@@ -19,4 +19,18 @@ router.put("/:id", async(req, res) => {
     
 })
 
+// ユーザー情報の削除
+router.delete("/:id", async(req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            const user = await User.findByIdAndDelete(req.params.id)
+            res.status(200).json("ユーザー情報を削除しました。")
+        } catch (error) {
+           return res.status(500    ).json(err);
+        }
+    } else {
+        return res.status(403).json("あなたは自分のアカウントのときだけ削除できます。");
+    }
+})
+
 module.exports = router;
